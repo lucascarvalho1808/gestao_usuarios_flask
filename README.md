6d0e249b209be08335aa1b55877d4045c6fdbdec
Lucas Carvalho <lucas.carvalho180807@gmail.com>
2025-09-16 08:03:17 -0300

    commit da versao inicial do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# gestao_usuarios
+
+Projeto de gestão de usuários usando Python e Flask 
+
+### install
+```
+$ pip install -r requirements.txt
+```
+
+### run
+```
+$ python main.py
+```
---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id": 1, "nome": "Guilherme", "email": "santos@gmail.com"},
+    {"id": 2, "nome": "Maria", "email": "maria.oliveira@hotmail.com"},
+    {"id": 3, "nome": "João", "email": "joao.silva@yahoo.com"},
+    {"id": 4, "nome": "Ana", "email": "ana.costa@outlook.com"},
+    {"id": 5, "nome": "Pedro", "email": "pedro.souza@gmail.com"},
+    {"id": 6, "nome": "Carla", "email": "carla.p@uol.com.br"},
+]
